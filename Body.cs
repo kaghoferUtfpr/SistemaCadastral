d12f58b1281897f6a88b52fb23aca447449fb102
Daniel Kaghoer <kaghofer@gmail.com>
2023-03-27 22:28:50 -0300

    Add project files.

---
+++ Body.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCadastral
+{
+    public class Body
+    {
+        public static void TelaMenu()
+        {
+            Console.WriteLine();
+        }
+        public static void TelaCadastro()
+        {
+            string l1 = "Registro....: 1 de <N>";
+            string l2 = "Nome........:";
+            string l3 = "RG..........:";
+            string l4 = "CPF.........:";
+            string l5 = "Habilitação.:";
+            string l6 = "Tit. Eleitor:";
+            Console.WriteLine(l1);
+            Console.WriteLine(l2);
+            Console.WriteLine(l3);
+            Console.WriteLine(l4);
+            Console.WriteLine(l5);
+            Console.WriteLine(l6);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SistemaCadastral
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Header.Imprimir();
+
+            string menu = "\nF1 – Cadastrar documentos\r\nF2 – Pesquisar documentos\r\nF3 – Listar documentos\r\n\nF7 – Carregar documentos\r\nF8 – Salvar documentos\r\n\nF9 – Sair";
+            Console.WriteLine(menu);
+
+
+        }
+    }
+}
